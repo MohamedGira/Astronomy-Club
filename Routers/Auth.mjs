bb6b1a2987d598bf3086c077071194d5a670cbfc
MohamedGira <mohamedgira0901@gmail.com>
2023-03-20 23:26:44 +0200

    added password resetting routes

--- Routers/Auth.mjs
+++ Routers/Auth.mjs
@@ -4,6 +4,7 @@ import {login} from "../controllers/Authentication/login.mjs";
 import {logout} from "../controllers/Authentication/logout.mjs";
 import { isLoggedIn } from "../controllers/Authentication/AuthUtils.mjs";
 import * as regisrationController from "../controllers/Authentication/register.mjs";
+import * as resetPasswordController from "../controllers/Authentication/resetPassword.mjs";
 
 export const AuthRouter=express.Router()
 AuthRouter.route('/register').post(isLoggedIn,catchAsync(regisrationController.registerUser))
@@ -13,5 +14,7 @@ AuthRouter.route('/login').post(isLoggedIn,catchAsync(login))
 AuthRouter.route('/logout').post(logout)
 
 
+AuthRouter.route('/resetPassword').post(isLoggedIn,catchAsync(resetPasswordController.resetPassword))
+AuthRouter.route('/changePassword').post(catchAsync(resetPasswordController.changePassword))
 
 
