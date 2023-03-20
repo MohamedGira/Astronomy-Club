f40a89873f6d56d9eaeaa4b011ace3cc86e5acc2
MohamedGira <mohamedgira0901@gmail.com>
2023-03-20 22:16:28 +0200

    added login/logout controllers routes

--- Routers/Auth.mjs
+++ Routers/Auth.mjs
@@ -1,11 +1,17 @@
 import express from "express";
+import {catchAsync} from "../utils/catchAsync.mjs";
+import {login} from "../controllers/Authentication/login.mjs";
+import {logout} from "../controllers/Authentication/logout.mjs";
+import { isLoggedIn } from "../controllers/Authentication/AuthUtils.mjs";
 import * as regisrationController from "../controllers/Authentication/register.mjs";
-import { catchAsync } from "../utils/catchAsync.mjs";
-
 
 export const AuthRouter=express.Router()
-AuthRouter.route('/register').post(catchAsync(regisrationController.registerUser))
+AuthRouter.route('/register').post(isLoggedIn,catchAsync(regisrationController.registerUser))
 AuthRouter.route('/confirmRegistration').get(catchAsync(regisrationController.confirmRegisteration))
+
+AuthRouter.route('/login').post(isLoggedIn,catchAsync(login))
+AuthRouter.route('/logout').post(logout)
+
 
 
 
