67cd62e5c5759e319db87b45bd7e08f0acb8c6c3
MohamedGira <mohamedgira0901@gmail.com>
2023-03-22 23:30:12 +0200

    added online DB

--- models/DbConnection.js
+++ models/DbConnection.js
@@ -1,19 +1,24 @@
 import mongoose from'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
-
-const localDB = "mongodb://127.0.0.1:27017/Astronomy"
+/* const localDB=''
+const DB=''
+if (process.env.NODE_ENV=='localdevelopment')
+     localDB = "mongodb://127.0.0.1:27017/Astronomy" */
+const DB= `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSCDE}@cluster0.sq0pkrl.mongodb.net/astronomy?retryWrites=true&w=majority`
+console.log(DB)
 mongoose.set('strictQuery', false);
 
 export const connectDb= async()=>{
     console.log("connecting to db")
-    await mongoose.connect(localDB,{
+    await mongoose.connect(DB,{
         useNewUrlParser:true,
         useUnifiedTopology:true   
     })
 
     console.log("Db connected")
 }
-
 
 
 
