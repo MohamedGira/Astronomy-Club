e7176460c5cbbcab3e59d0909d3fb05aaedc3279
MohamedGira <mohamedgira0901@gmail.com>
2023-03-23 16:17:42 +0200

    enhanced authorizer performance

--- controllers/Authentication/authorizationMw.mjs/Authorizer.mjs
+++ controllers/Authentication/authorizationMw.mjs/Authorizer.mjs
@@ -11,14 +11,22 @@ export const isAuthorized= (role)=>{
     return   catchAsync (async (req,res,next)=>{
         const token = req.cookies.jwt;
         const verifypromisfied=promisify (jwt.verify);
+        try{
         const decodedValues= await verifypromisfied (token, process.env.JWT_KEY)
-        
+
+        //unnecessary logic
         if (!decodedValues.email)
             return next(new AppError(401, "invalid request"));
-        const user=await User.findOne({email:decodedValues.email}).populate('role')
-        if (user.role.role!==role)
+        //refactoring: too much overhead, just use the role provided in the jwt
+        //const user=await User.findOne({email:decodedValues.email}).populate('role')
+        //if (user.role.role!==role)
+        if (decodedValues.role!==role)
             return next(new AppError(401, "unauthorized access to this endpoint"));
         next()
+         }
+        catch(err){
+            return next(new AppError(401, "unauthorized access to this endpoint"))
+        }
         })
     }
     
