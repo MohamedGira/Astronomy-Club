44730750f077ebba2aa8a71dc6b261679337fa5b
MohamedGira <mohamedgira0901@gmail.com>
2023-03-25 01:53:13 +0200

    added protect route middleware

--- controllers/Authentication/AuthUtils.mjs
+++ controllers/Authentication/AuthUtils.mjs
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import { User } from '../../models/Users/User.mjs';
 import { AppError } from '../../utils/AppError.mjs';
+import { catchAsync } from '../../utils/catchAsync.mjs';
 
 export const isLoggedIn = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -12,4 +15,19 @@ export const isLoggedIn = (req, res, next) => {
         return next(new AppError(401, "a user is already logged in to this device"));
       else return next();
     });
+}
+
+
+export const protect =catchAsync(async (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (!token)
+   return next(new AppError(401, "signin to continue"));
+  
+  const decodedvalues=await promisify( jwt.verify)(token, process.env.JWT_KEY)
+  const user= await User.findById(decodedvalues.id)
+  if(!user){
+    return next(new AppError(401, "user with this token no longer exists"));
   }
+  req.user=user
+  next()
+})
