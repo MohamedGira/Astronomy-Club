a8fea3d200052ac0efd706bc8ad0b9300f5a0c32
MohamedGira <mohamedgira0901@gmail.com>
2023-04-11 00:34:36 +0200

    newer much better saveImage technique: sharp

--- utils/uploads/ImageCompression/compressor.mjs
+++ utils/uploads/ImageCompression/compressor.mjs
@@ -39,9 +39,24 @@ class BufferCompressor{
     }
 }
 
-class ImageResizer  {
+class ImageHandler  {
     constructor(){
         
+    }
+     saveImage(image,options={compress:false,maxHeight:1080,maxWidth:1920}){
+         
+         let img=sharp(image)
+        .toFormat('jpeg')
+        if(options.compress)
+        {
+        options.maxHeight=options.maxHeight||1080
+        options.maxWidth=options.maxWidth||1920
+        img.resize(options.maxWidth, options.maxHeight, {
+            fit: sharp.fit.inside,
+            withoutEnlargement: true
+        })
+        }
+        return img
     }
     async resizeImage(image,maxHeight=1080,maxWidth=1920){    
         return await sharp(image)
@@ -49,10 +64,12 @@ class ImageResizer  {
             fit: sharp.fit.inside,
             withoutEnlargement: true
         })
+        .toFormat('jpeg')
         .toBuffer()
+        
     }
 }
 
 
 export const bufferCompressor=new BufferCompressor()
-export const imageResizer=new ImageResizer()
+export const imageHandler=new ImageHandler()
--- utils/uploads/saveImage.mjs
+++ utils/uploads/saveImage.mjs
@@ -1,5 +1,5 @@
 import { writeFileSync } from "fs";
-import { imageResizer, bufferCompressor } from "../../utils/uploads/ImageCompression/compressor.mjs";
+import { imageHandler, bufferCompressor } from "../../utils/uploads/ImageCompression/compressor.mjs";
 import { AppError } from "../../utils/AppError.mjs";
 import { fileURLToPath } from "url";
 import path from "path";
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 import fs from 'fs'
 export const  imgdir=(__dirname+'/../../upload/images/').replace(/\\/g, "/")
 /** options :{subfolder:String,compress:boolean} */
-export const saveImage = async function (image,options={subfolder:'',compress:false})
+export const saveImageOld = async function (image,options={subfolder:'',compress:false})
  {
 
   if (!fs.existsSync(imgdir)){
@@ -25,7 +25,7 @@ export const saveImage = async function (image,options={subfolder:'',compress:fa
 
   //compressing the image
   if (options.compress  == true) {
-    const compressedImg = await imageResizer.resizeImage(image.data);
+    const compressedImg = await ImageHandler.resizeImage(image.data);
     writeFileSync(imgdir+ imgname, compressedImg, "binary");
   }else{
     writeFileSync(imgdir+ imgname, image.data, "binary");
@@ -35,6 +35,27 @@ export const saveImage = async function (image,options={subfolder:'',compress:fa
   return imgname;
 
 };
+
+
+export const saveImage = async function (image,options={subfolder:'',compress:false,maxHeight:1080,maxWidth:1920})
+ {
+
+  //uploaded file is not image
+  if (image && !/^image/.test(image.mimetype))
+    throw new AppError("400","the provided file's extension is not a supported image type");
+  let fdir=imgdir
+  if(options.subfolder)
+    fdir= path.join(imgdir,options.subfolder)
+
+  if (!fs.existsSync(fdir)){
+    fs.mkdirSync(fdir,{ recursive: true });
+  }
+  
+  const imgname = `${Date.now()}${parseInt(Math.random() * 1000).toString()}.jpeg`;
+  await imageHandler.saveImage(image.data,options).toFile(path.join(imgdir,imgname));
+  return imgname;
+};
+
 
 export const createImageObject = async function (image,compress = false)
  {
