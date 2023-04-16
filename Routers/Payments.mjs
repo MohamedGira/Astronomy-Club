6fdd11f59cce4a1fd3e2f951794f3d7ecd1c3142
MohamedGira <mohamedgira0901@gmail.com>
2023-04-16 17:08:32 +0200

    Refactoring: namings

---
+++ Routers/Payments.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import { isAuthorizedMw } from "../controllers/Authentication/authorizationMw.mjs/Authorizer.mjs";
+
+
+export const PaymentRouter=express.Router()
+
+import * as PaymentsController from "../controllers/Booking/CRUDPayment.mjs"
+import { protect } from "../controllers/Authentication/AuthUtils.mjs";
+
+PaymentRouter.use(protect)
+PaymentRouter.route('/')
+.get(PaymentsController.getPayments)
+.post(isAuthorizedMw('admin'),PaymentsController.addPayment)
+
+PaymentRouter.route('/:elementId')
+.get(PaymentsController.getPayment)
+.patch(isAuthorizedMw('admin'),PaymentsController.updatePayment)
+.delete(isAuthorizedMw('admin'),PaymentsController.deletePayment)
