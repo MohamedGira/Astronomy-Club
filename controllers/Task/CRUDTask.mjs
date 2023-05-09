62e7ce41fd7f47c00ebb71616169ad8e043896c6
helmy162 <104876574+helmy162@users.noreply.github.com>
2023-05-09 17:38:01 +0300

    refactoring and removing unused code

--- controllers/Task/CRUDTask.mjs
+++ controllers/Task/CRUDTask.mjs
@@ -1,16 +1,5 @@
 import { Task } from "../../models/Tasks/Task.mjs";
-import {filterObj,jsonifyObj} from "../../utils/objOp.mjs"
 import * as factory from "../CRUDFactory.mjs";
-
-
-export const createTask= async(unfilteredBody)=>{
-    
-    unfilteredBody=jsonifyObj(unfilteredBody)
-    var taskBody=filterObj(unfilteredBody,Task.schema.paths) 
-    var newTask=await Task.create({...taskBody})
-    return newTask
-}
-
 
 // GET tasks/
 export const  getTasks= factory.getAll(Task)
