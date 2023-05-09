85d38b5b5cf576f7d190c62fb8ea0aa4f97989a4
helmy162 <104876574+helmy162@users.noreply.github.com>
2023-05-09 18:19:09 +0300

    added myTasks controller & router allow user to see his tasks

--- Routers/Users.mjs
+++ Routers/Users.mjs
@@ -6,6 +6,7 @@ import { confirmUser } from "../controllers/User/Admin.mjs/confirmMember.mjs";
 import { getUser } from "../controllers/User/getUser.mjs";
 import { getusers } from "../controllers/User/getUsers.mjs";
 import { myProfile } from "../controllers/User/myProfile.mjs";
+import { myTasks } from "../controllers/User/myTasks.mjs";
 import {catchAsync} from "../utils/catchAsync.mjs";
 
 export const UserRouter=express.Router()
@@ -13,6 +14,7 @@ export const UserRouter=express.Router()
 UserRouter.post('/confirmUser',isAuthorizedMw('admin'),confirmUser)
 
 UserRouter.get('/myProfile',protect,myProfile)
+UserRouter.get('/myTasks', protect,myTasks)
 UserRouter.get('/',isAuthorizedMw('admin'),getusers)
 
 UserRouter.get('/:id',isAuthorizedMw('admin'),getUser)
