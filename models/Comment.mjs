ace6df5afa26309fc81929aa4dae53322d4ce14f
MohamedGira <mohamedgira0901@gmail.com>
2023-05-11 23:17:24 +0300

    updated comments model

--- models/Comment.mjs
+++ models/Comment.mjs
@@ -4,7 +4,19 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const commentSchema = mongoose.Schema({
-    content:String,
+    
+    content:{
+        type:String,
+        required:true,
+        validate:{
+            validator: async function(content){
+                if(content.trim().length==0)
+                    return false
+                return true
+            },
+            message:`can't add empty comment`
+        }    
+    },
     by: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
@@ -14,9 +26,17 @@ const commentSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         required:true
     },    
+    history:[],
+    deleted:{
+        type:Boolean,
+        default:false
+    }
 }, { timestamps: true });
 
-
+commentSchema.pre(/find/, function() {
+    this.where({deleted: {$ne:true}});
+    this.select('-deleted')
+});
   
 
 
