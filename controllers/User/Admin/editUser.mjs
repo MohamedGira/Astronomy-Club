c9f969642a998729ad106fb89531cc0f35065ad1
MohamedGira <mohamedgira0901@gmail.com>
2023-05-25 14:30:18 +0300

    fixed N/A assignment for committees

--- controllers/User/Admin/editUser.mjs
+++ controllers/User/Admin/editUser.mjs
@@ -1,4 +1,5 @@
 import { User } from "../../../models/Users/User.mjs"
+import { AppError } from "../../../utils/AppError.mjs"
 import { catchAsync } from "../../../utils/catchAsync.mjs"
 import { filterObj } from "../../../utils/objOp.mjs"
 import { saveImage } from "../../../utils/uploads/saveImage.mjs"
@@ -6,8 +7,13 @@ import { saveImage } from "../../../utils/uploads/saveImage.mjs"
 export const editUser = catchAsync( async (req, res, next) => {    
     
     let body=filterObj(req.body,User.schema.paths, ['password','profileImage'])
-    
+    if(body.committee=='N/A')
+        body.committee=null
     let user=await User.findByIdAndUpdate(req.params.id,body,{new:true})
+    if(!user)
+        return next(new AppError(404,'user not found'))
+    
+    
     if (req.files){
         console.log(user)
         if (req.files.profileImage)
