f4ebfa7d0034223f4659ba01d972f3994bae7f60
MohamedGira <mohamedgira0901@gmail.com>
2023-05-31 19:39:36 +0300

    added kanban router to the app

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ import slugify from "slugify";
 import { PermissionRouter } from "./Routers/Permissions.mjs";
 import { EndpointRouter } from "./Routers/Endpoints.mjs";
 import { InitializeEndpoints2 } from "./controllers/Endpoint/EndpointController.mjs";
+import { gatheringPointsRouter } from "./routers/GatheringPoints.mjs";
+import { KanbanRouter } from "./routers/Kanbans.mjs";
 
 process.on('uncaughtException',err=>{
     console.trace(`Error: ${err}`)
@@ -95,22 +97,20 @@ app.use('/api/v1/payments/',PaymentRouter)
 app.use('/api/v1/eventTypes/',eventTypesRouter)
 app.use('/api/v1/assignments/',AssignmentRouter)
 app.use('/api/v1/checkpoints/',CheckpointsRouter)
+app.use('/api/v1/gatheringPoints/',gatheringPointsRouter)
 app.use('/api/v1/tasks/',TaskRouter)
 app.use('/api/v1/assignments/',AssignmentRouter)
 app.use('/api/v1/boardColumns/',BoardColumnRouter)
 app.use('/api/v1/frontendManagment/',FrontendManagmentRouter)
 app.use('/api/v1/permissions/',PermissionRouter)
 app.use('/api/v1/endpoints/',EndpointRouter)
-
+app.use('/api/v1/kanbans',KanbanRouter)
 
 
      
-/* app.post('/uploadImage',catchAsync ( async (req,res,next)=>{
-   let name=await saveImage(req.files.image,{compress:true,subfolder:'a/a/'})
-   return res.json(name)
-})) */
 
-app.get('images/*',(req,res,next)=>{
+
+app.get('/images/*',(req,res,next)=>{
     return next(new AppError(404,`requested image not found :${req.path},${req.method}`))
 })
 app.all('*',(req,res,next)=>{
@@ -118,39 +118,38 @@ app.all('*',(req,res,next)=>{
 })
 app.use(ErrorHandler)
 
- 
-// a trick to stay up on the deployed site
-let stayup=(await deploymentTrick.findOne())
-console.log(stayup)
-var refreshEveryMins=stayup.refreshEvery||12
-setInterval(async () => {
-    stayup=(await deploymentTrick.findOne())
-    if(stayup)
-        stayup=stayup._doc
-    console.log(stayup.stayup)
-    if(stayup.stayup){ 
-        refreshEveryMins=stayup.refreshEvery||12
-        await fetch(`${stayup.siteUrl}/`).catch(err=>{
-            console.log(`couldn't send to ${stayup.siteUrl}/  ,  ${err.message}`)
-        })
-    }        
-},refreshEveryMins*60000) 
+try{
+  await Database.getInstance();
+  const server=  await app.listen(process.env.PORT, () =>{ console.log(`connected on port ${process.env.PORT}`)})
+  let stayup=(await deploymentTrick.findOne())
+  var refreshEveryMins=stayup.refreshEvery||12
+  setInterval(async () => {
+      stayup=(await deploymentTrick.findOne())
+      if(stayup)
+          stayup=stayup._doc
+      console.log(stayup.stayup)
+      if(stayup.stayup){ 
+          refreshEveryMins=stayup.refreshEvery||12
+          await fetch(`${stayup.siteUrl}/`).catch(err=>{
+              console.log(`couldn't send to ${stayup.siteUrl}/  ,  ${err.message}`)
+          })
+      }        
+  },refreshEveryMins*60000) 
+  //InitializeEndpoints2(app)
+
+  }catch(err){
+      console.log(err)
+ }
+  
+
  
 
-try{
-await Database.getInstance();
-const server=  await app.listen(process.env.PORT, () =>{ console.log(`connected on port ${process.env.PORT}`)})
-}catch(err){
-    console.log(err)
-}
 
 //saftey net
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.name}. ${err.message}`);
   console.log("Uhnandled Rejection", err);
-  server.close(() => {
-    process.exit(1);
-  });
+
 });
 
 
