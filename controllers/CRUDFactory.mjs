448b255c0d21b60727fea3d098d2ed32eaff9735
MohamedGira <mohamedgira0901@gmail.com>
2023-05-31 19:34:55 +0300

    Moved single/multi Image saving to the factory

--- controllers/CRUDFactory.mjs
+++ controllers/CRUDFactory.mjs
@@ -3,9 +3,10 @@ import { filterObj, jsonifyObj } from "../utils/objOp.mjs"
 import { catchAsync } from "../utils/catchAsync.mjs"
 import { AppError } from "../utils/AppError.mjs"
 import { ResultsManager } from "../utils/ResultsManager.mjs"
+import { saveImage } from "../utils/uploads/saveImage.mjs"
+import { deleteFile } from "../utils/uploads/cleanDir.mjs"
 //crud factory for BASIC classes : no childReferencing Docs
 
-//!!!!!!! if used model has images, then images must be saved on model file not controller
 
 /*
 *this is only for plain endpoints : for example api/v1/events/:elementId
@@ -18,7 +19,7 @@ import { ResultsManager } from "../utils/ResultsManager.mjs"
 
 
 export const CreateOne=(Model,populate=undefined,options={executePost:()=>{},
-executePre:[()=>{}]})=>{
+executePre:[async()=>{}]})=>{
     return catchAsync( async (req,res,next)=>{
         var filteredBody=filterObj(jsonifyObj(req.body),Model.schema.paths) 
         if(options.executePre)
@@ -28,11 +29,41 @@ executePre:[()=>{}]})=>{
                 catch(err){
                     return next(err)
                 }
-
-        var filteredFiles;
-        if (req.files)
-            filteredFiles=filterObj(jsonifyObj(req.files),Model.schema.paths) 
-            var newModelObject=await Model.create({...filteredBody,...filteredFiles})
+        
+        //creating the model
+        var newModelObject=await Model(filteredBody)
+                 
+         //Handling images saving    
+         let imgslist=[]
+         try{
+             for (let key in req.files){
+                 //case1: model has an array of images
+                 if(Model.schema.paths[key].instance=='Array'){
+                    if(!Array.isArray(req.files[key]))
+                         req.files[key]=[req.files[key]]
+                    for (let img in req.files[key]){
+                        let imgname=await saveImage(req.files[key][img])
+                        newModelObject[key].push(imgname)
+                        imgslist.push(imgname)
+                        }
+                 //case2: model has a single image
+                 }else if(Model.schema.paths[key].instance=='String'){
+                    let imgname=await saveImage(req.files[key])
+                     newModelObject[key]=imgname
+                     imgslist.push(imgname)
+                 }
+            }
+            await newModelObject.save()
+         }catch(err){
+             try{
+                await Model.findByIdAndDelete(newModelObject._id)
+                imgslist.forEach(el=>deleteFile(el,'images'))
+             }catch(e){}
+             console.log(`couldn\'t create ${Model.collection.collectionName}, imgs issue`)
+             return next(new AppError(400,'image saving issue: '+err.message))
+         }
+        
+        //populating the model
         if (populate)
             await newModelObject.populate(populate.join(' '))
         if(options.executePost)
@@ -46,36 +77,47 @@ executePre:[()=>{}]})=>{
 
 /** params req.params: elementId -> referes to the requested document */
 export const getOne=(Model,populate=[],options={executePost:()=>{},
-executePre:[()=>{}]})=>{
+executePre:[()=>{}],showDeleted:false},name=undefined)=>{
+
     return catchAsync(  async (req,res,next)=>{
+        //fetching the element
+        
         const elementId=req.params.elementId
         var modelObject=Model.findOne({_id:elementId})
-        populate.forEach(el=>{console.log(el);modelObject.populate(el)})
+        //populating the element
+        if (populate)
+            modelObject.populate(populate.join(' '))
+        //checking if must show deleted
+        if(options.showDeleted)
+            modelObject.where({'elementStatus.isDeleted':true})
+        else
+            modelObject.where({'elementStatus.isDeleted':{$ne:true}})
+
+        //executing the query
         modelObject=await modelObject
 
         if(!modelObject)
             return next(new AppError(404,`requested ${Model.collection.collectionName} of id ${elementId} doesn\'t exitst`))
-    
+        if(!name)
+            name=Model.collection.collectionName
         return res.status(200).json({
-            message:`${Model.collection.collectionName} found`,
+            message:`${name} found`,
             modelObject
         })
 })}
 
 /** params req.params: elementId -> referes to the  document to be updated */
+/** Image saving must be handled in executePres */
 export const updateOne=(Model,filterout=[],options={executePost:()=>{},
-executePre:[()=>{}]})=>{
+executePre:[async()=>{}]})=>{
     return catchAsync( async (req,res,next)=>{
-        jsonifyObj(req.body)
         if(options.executePre)
-            for (let i in options.executePre)
-                {   
+            for (let i in options.executePre)     
                     await options.executePre[i](req,res,next)
-                }
-        var filteredFiles;
-        if (req.files)
-            filteredFiles=filterObj(jsonifyObj(req.files),Model.schema.paths,filterout) 
-        var update={...filterObj(req.body,Model.schema.paths),...filteredFiles}
+        
+        console.log(jsonifyObj(req.body))
+
+        var update={...filterObj(jsonifyObj(req.body),Model.schema.paths,...filterout)}
         const elementId=req.params.elementId
         let newModelObject= await Model.findByIdAndUpdate(elementId,update,{
             new:true,
@@ -87,7 +129,6 @@ executePre:[()=>{}]})=>{
         return next( new AppError(400,`requested ${Model.collection.collectionName} of id ${elementId} doesn\'t exitst`))
     }
     if(options.executePost)
-        
         options.executePost()
         
     return res.status(200).json({
@@ -100,9 +141,13 @@ executePre:[()=>{}]})=>{
 export const deleteOne=(Model)=>{
     return catchAsync( async (req,res,next)=>{
         const id=req.params.elementId
-        const doc = await Model.findByIdAndDelete(id)
+        const doc = await Model.findById(id)
+        
         if(!doc)
-        return next(new AppError(404,`requested document ${id} doesn't exitst`))
+            return next(new AppError(404,`requested document ${id} doesn't exitst`))
+        doc.elementStatus.isDeleted=true
+        doc.elementStatus.deletedBy=req.user._id
+        await doc.save()
         return res.status(204).json({
             message:'deleted succesfully',
             doc
@@ -111,9 +156,8 @@ export const deleteOne=(Model)=>{
 )}
 
 /** params None, filter: filteres the resources for requested id */
-
-export const getAll=(Model,populate=[],options={executePost:()=>{},
-executePre:[()=>{}]})=>{
+export const getAll=(Model,populate=[],
+    options={executePost:()=>{},executePre:[()=>{}],showDeleted:false,onlyOne:false},name=undefined)=>{
     return catchAsync( async (req,res,next)=>{
         if(options.executePre)
             for (let i in options.executePre)    
@@ -123,13 +167,24 @@ executePre:[()=>{}]})=>{
         const elementId=req.params.elementId
         var results;
             results = new ResultsManager(Model.find(),req.query).filter().select().paginate().query
+        //checking if must show deleted
+        if(options.showDeleted)
+            results.where({'elementStatus.isDeleted':true})
+        else
+            results.where({'elementStatus.isDeleted':{$ne:true}})
+        //populating requested fields
         if (populate)
             results.populate(populate.join(' '))
+
         results=await results
+        if(!name)
+            name=Model.collection.collectionName
         if(!results)
-            return next(new AppError(404,`requested ${Model.collection.collectionName} of id ${elementId} doesn\'t exitst`))
+            return next(new AppError(404,`requested ${name} of id ${elementId} doesn\'t exitst`))
+        if(options.onlyOne && results.length==1)
+            results=results[0]
         return res.status(200).json({
-            message:`${results.length} ${Model.collection.collectionName} found`,
+            message:`${results.length} ${name} found`,
             results
         })
 })}
